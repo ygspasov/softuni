f9e9eecb89600cfa08ae90a0a0107a80df50ce3b
ygspasov <ygspasov@gmail.com>
2017-02-07 17:58:20 +0200

    Active class
    Adding class "active" to navigation.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 /**
- * Created by Yavor on 31/01/2017.
- */
-$('.nav li a').click(function(e) {
-    var $this = $(this);
-    if (!$this.hasClass('active')) {
-        $this.addClass('active');
-    }
-    e.preventDefault();
+* Created by Yavor on 31/01/2017.
+*/
+jQuery(function() {
+    // jQuery('li').click( function() {
+    //     jQuery(this).addClass('active').siblings().removeClass('active');
+    // });
+
+    //highlight the current nav
+    $("#index a:contains('Вкъщи')").parent().addClass('active');
+    $("#winners a:contains('Победители')").parent().addClass('active');
+    $("#team a:contains('Екип')").parent().addClass('active');
 });
