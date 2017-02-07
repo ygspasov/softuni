d4a87a6bee5a2235e6dcf5b07e61a8b11e3cb6bd
ygspasov <ygspasov@gmail.com>
2017-02-07 05:22:42 +0200

    Social icons
    Adding social icons from Font Awesome and 2 more pages.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 /**
  * Created by Yavor on 31/01/2017.
  */
+$('.nav li a').click(function(e) {
+    var $this = $(this);
+    if (!$this.hasClass('active')) {
+        $this.addClass('active');
+    }
+    e.preventDefault();
+});
